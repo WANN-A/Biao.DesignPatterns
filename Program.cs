93e9865064a867cfa904aa1f25c994536dca23b9
WangBiao <1513119437@qq.com>
2023-04-24 21:32:50 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Biao.DesignPatterns {
+    internal class Program {
+        static void Main(string[] args) {
+            //单例模式
+            Biao.DesignPatterns.CreationalPatterns.SingletonPattern singletonPattern =
+                Biao.DesignPatterns.CreationalPatterns.SingletonPattern.GetInstance();
+            singletonPattern.ToString();
+        }
+    }
+}

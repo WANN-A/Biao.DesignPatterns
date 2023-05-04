0ea60b691c4c8913b42d64854c95ce237999bdc0
Biao Wang <wangbiao333@stu.xhu.edu.cn>
2023-05-04 20:37:45 +0800

    [Title] 添加工厂模式示例

---
+++ CreationalPatterns/FactoryPattern.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biao.DesignPatterns.CreationalPatterns {
+    internal class FactoryPattern {
+    }
+
+    /// <summary>
+    /// 定义一个图形接口
+    /// </summary>
+    public interface IShape {
+        void Draw();
+    }
+
+    /// <summary>
+    /// 定义实现图形接口的矩形实体类
+    /// </summary>
+    public class Rectangle : IShape {
+        public void Draw() {
+            Console.WriteLine("画矩形。");
+        }
+    }
+
+    /// <summary>
+    /// 定义实现图形接口的圆形实体类
+    /// </summary>
+    public class Circle : IShape {
+        public void Draw() {
+            Console.WriteLine("画圆形。");
+        }
+    }
+
+    /// <summary>
+    /// 定义实现图形接口的三角形实体类
+    /// </summary>
+    public class Triangle : IShape {
+        public void Draw() {
+            Console.WriteLine("画三角形。");
+        }
+    }
+
+    /// <summary>
+    /// 创建Shape的工厂类，通过传递类型信息来获取实体类的对象。
+    /// </summary>
+    public class ShapeFactory {
+        public IShape GetShape(string ShapeType) {
+            if (ShapeType == null) {
+                return null;
+            } else if (ShapeType == "Rectangle") {
+                return new Rectangle();
+            } else if (ShapeType == "Circle") {
+                return new Circle();
+            } else if (ShapeType == "Triangle") {
+                return new Triangle();
+            }
+            return null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Biao.DesignPatterns {
+﻿using Biao.DesignPatterns.CreationalPatterns;
+
+namespace Biao.DesignPatterns {
     internal class Program {
         static void Main(string[] args) {
-            //单例模式
+            //测试单例模式
             Biao.DesignPatterns.CreationalPatterns.SingletonPattern singletonPattern =
                 Biao.DesignPatterns.CreationalPatterns.SingletonPattern.GetInstance();
             singletonPattern.ToString();
+
+            //测试工厂模式
+            Biao.DesignPatterns.CreationalPatterns.ShapeFactory shapeFactory = new CreationalPatterns.ShapeFactory();
+            IShape shape1 = shapeFactory.GetShape("Rectangle");
+            IShape shape2 = shapeFactory.GetShape("Circle");
+            IShape shape3 = shapeFactory.GetShape("Triangle");
+            shape1.Draw();
+            shape2.Draw();
+            shape3.Draw();
         }
     }
 }

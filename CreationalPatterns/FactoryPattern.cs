28645b6ac1ec3a1b211be34e5a228df2173c70d7
Biao Wang <wangbiao333@stu.xhu.edu.cn>
2023-05-04 20:49:08 +0800

    工厂模式示例代码

--- CreationalPatterns/FactoryPattern.cs
+++ CreationalPatterns/FactoryPattern.cs
@@ -5,8 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Biao.DesignPatterns.CreationalPatterns {
-    internal class FactoryPattern {
-    }
+    //internal class FactoryPattern {
+    //}
 
     /// <summary>
     /// 定义一个图形接口
